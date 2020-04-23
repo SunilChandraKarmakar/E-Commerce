6c3a36c04d78500ff199025c8eaeef9dba099e2b
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-04-23 20:19:56 +0600

    ECommerce
    1. Create IAccountRepository, IRoleAccountRepository interface, AccountRepository and RoleAccountRepository class in CompleteECommarce.Repository project.
    2. Create IAccountManager, IRoleAccountManager interface, AccountManager and RoleAccountManager class in CompleteECommarce.Manager project.

---
+++ CompletedECommerce/CompletedECommerce.Manager/AccountManager.cs
@@ -0,0 +1,20 @@
+﻿using CompletedECommerce.Manager.Base;
+using CompletedECommerce.Manager.Contracts;
+using CompletedECommerce.Repository.Contracts;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompletedECommerce.Manager
+{
+    public class AccountManager : BaseManager<Account>, IAccountManager
+    {
+        private readonly IAccountRepository _iAccountRepository;
+
+        public AccountManager(IAccountRepository iAccountRepository) : base(iAccountRepository)
+        {
+            _iAccountRepository = iAccountRepository;
+        }
+    }
+}
---
+++ CompletedECommerce/CompletedECommerce.Manager/RoleAccountManager.cs
@@ -0,0 +1,20 @@
+﻿using CompletedECommerce.Manager.Base;
+using CompletedECommerce.Manager.Contracts;
+using CompletedECommerce.Repository.Contracts;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompletedECommerce.Manager
+{
+    public class RoleAccountManager : BaseManager<RoleAccount>, IRoleAccountManager 
+    {
+        private readonly IRoleAccountRepository _iRoleAccountRepository;
+
+        public RoleAccountManager(IRoleAccountRepository iRoleAccountRepository) : base(iRoleAccountRepository)
+        {
+            _iRoleAccountRepository = iRoleAccountRepository;
+        }
+    }
+}
