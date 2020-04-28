34a55d7be1698c77bf4985806f87616564561b15
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-04-28 20:05:22 +0600

    ECommerce
    1. Create SearchViewComponent and view in _ClinteLayout.

---
+++ CompletedECommerce/CompletedECommerce/ViewComponents/SearchViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CompletedECommerce.Manager.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompletedECommerce.ViewComponents
+{
+    public class SearchViewComponent : ViewComponent 
+    {
+        private readonly ICategoryManager _iCategoryManager;
+
+        public SearchViewComponent(ICategoryManager iCategoryManager)
+        {
+            _iCategoryManager = iCategoryManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ICollection<Category> categoryList = _iCategoryManager.GetAll()
+                                                .Where(c => c.Categorye == null & c.Status == true).ToList();
+            return View(categoryList);
+        }
+    }
+}
