50a2dd56c5a888d734f61cd729c0471e1a3de1b0
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-04-29 21:54:13 +0600

    ECommerce
    1. Create SlideShowViewComponent class.
    2. Create SlideShowViewComponent view.
    2. Working with _ClinteLayout.

---
+++ CompletedECommerce/CompletedECommerce/ViewComponents/SlideShowViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using CompletedECommerce.Manager.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompletedECommerce.ViewComponents
+{
+    public class SlideShowViewComponent : ViewComponent 
+    {
+        private readonly ISlideShowManager _iSlideShowManager;
+
+        public SlideShowViewComponent(ISlideShowManager iSlideShowManager)
+        {
+            _iSlideShowManager = iSlideShowManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ICollection<SlideShow> slideShowList = _iSlideShowManager.GetAll().Where(s => s.Status == true).ToList();
+            return View(slideShowList);
+        }
+    }
+}
