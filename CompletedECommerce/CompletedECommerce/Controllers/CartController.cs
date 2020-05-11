e130303a08f51381173efb4233578370d3feb388
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-05-11 21:54:40 +0600

    Build Shopping Cart
    1. Create CartController.
    2. Create Index get function with view in CartController.
    3. Create CartTop and CartLeft ViewComponent.
    4. Working with _ClinteLayout.

---
+++ CompletedECommerce/CompletedECommerce/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompletedECommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("CustomerId") != null)
+                return View();
+
+            return RedirectToAction("CustomerLogin", "Login");
+        }
+    }
+}
