015c1d25de3cfd283454fc151f3b3f45810c1a07
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-05-17 20:38:35 +0600

    Repository and Manager
    1. Create InvoiceRepository, InvoiceDetailsRepository interface and class in ECommerce.Repository project.
    2. Create InvoiceManager, InvoiceDetailsManager interface and class in ECommerce.Manager project.

---
+++ CompletedECommerce/CompletedECommerce.Manager/InvoiceDetailsManager.cs
@@ -0,0 +1,21 @@
+﻿using CompletedECommerce.Manager.Base;
+using CompletedECommerce.Manager.Contracts;
+using CompletedECommerce.Repository.Base;
+using CompletedECommerce.Repository.Contracts;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompletedECommerce.Manager
+{
+    public class InvoiceDetailsManager : BaseManager<InvoiceDetails>, IInvoiceDetailsManager
+    {
+        private readonly IInvoiceDetailsRepository _iInvoiceDetailsRepository;
+
+        public InvoiceDetailsManager(IInvoiceDetailsRepository iInvoiceDetailsRepository) : base(iInvoiceDetailsRepository)
+        {
+            _iInvoiceDetailsRepository = iInvoiceDetailsRepository;
+        }
+    }
+}
---
+++ CompletedECommerce/CompletedECommerce.Manager/InvoiceManager.cs
@@ -0,0 +1,20 @@
+﻿using CompletedECommerce.Manager.Base;
+using CompletedECommerce.Manager.Contracts;
+using CompletedECommerce.Repository.Contracts;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompletedECommerce.Manager
+{
+    public class InvoiceManager : BaseManager<Invoice>, IInvoiceManager
+    {
+        private readonly IInvoiceRepository _iInvoiceRepository;
+
+        public InvoiceManager(IInvoiceRepository iInvoiceRepository) : base(iInvoiceRepository)
+        {
+            _iInvoiceRepository = iInvoiceRepository;
+        }
+    }
+}
