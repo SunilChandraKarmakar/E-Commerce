f3885d5e4f398a54ad99fede8db2d17b3ab9e90b
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-05-18 20:06:36 +0600

    Admin Dashboard Statistics
    1. Working with Index get function and view in DashboardController

--- CompletedECommerce/CompletedECommerce/Controllers/DashboardController.cs
+++ CompletedECommerce/CompletedECommerce/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompletedECommerce.Manager.Contracts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +10,37 @@ namespace CompletedECommerce.Controllers
 {
     public class DashboardController : Controller
     {
-       [HttpGet]
-       public IActionResult Index()
-       {
+        private readonly IInvoiceManager _iInvoiceManager;
+        private readonly IProductManager _iProductManager;
+        private readonly IRoleAccountManager _iRoleAccountManager;
+        private readonly ICategoryManager _iCategoryManager;
+
+        public DashboardController(IInvoiceManager iInvoiceManager, IProductManager iProductManager,
+                                    IRoleAccountManager iRoleAccountManager, 
+                                    ICategoryManager iCategoryManager)
+        {
+            _iInvoiceManager = iInvoiceManager;
+            _iProductManager = iProductManager;
+            _iRoleAccountManager = iRoleAccountManager;
+            _iCategoryManager = iCategoryManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
             if (HttpContext.Session.GetString("AdminId") != null)
+            {
+                ViewBag.NewOrder = _iInvoiceManager.GetAll()
+                                   .Where(i => i.Status == false).ToList();
+                ViewBag.TotalProducts = _iProductManager.GetAll();
+                ViewBag.TotalCustomer = _iRoleAccountManager.GetAll().
+                                        Where(ra => ra.RoleId == 2).ToList();
+                ViewBag.TotalCategory = _iCategoryManager.GetAll()
+                                        .Where(c => c.Categorye == null).ToList();
                 return View();
-            else
-                return RedirectToAction("Index", "Login");
-       }
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
     }
 }
